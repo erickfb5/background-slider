375dff4ce5e931248454bba5279ba3a3f13ded7f
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-01-24 22:34:27 -0300

    refactored handleClick

--- script.js
+++ script.js
@@ -1,40 +1,28 @@
-const body = document.body
-const slides = document.querySelectorAll('.slide')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
+const body = document.body;
+const slides = document.querySelectorAll(".slide");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
 
-let activeSlide = 0
+let activeSlide = 0;
 
-rightBtn.addEventListener('click', () => {
-  activeSlide++
+const handleClick = (direction) => {
+  activeSlide =
+    direction === "right"
+      ? (activeSlide + 1) % slides.length
+      : (activeSlide - 1 + slides.length) % slides.length;
+  setBgToBody();
+  setActiveSlide();
+};
 
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
+const setBgToBody = () =>
+  (body.style.backgroundImage = slides[activeSlide].style.backgroundImage);
 
-  setBgToBody()
-  setActiveSlide()
-})
+const setActiveSlide = () => {
+  slides.forEach((slide) => slide.classList.remove("active"));
+  slides[activeSlide].classList.add("active");
+};
 
-leftBtn.addEventListener('click', () => {
-  activeSlide--
+rightBtn.addEventListener("click", () => handleClick("right"));
+leftBtn.addEventListener("click", () => handleClick("left"));
 
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
-setBgToBody()
-
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
-}
+setBgToBody();
